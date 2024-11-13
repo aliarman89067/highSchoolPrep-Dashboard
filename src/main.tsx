aed17df65ad5ec8c09913ee73ca92955453ec1cb
Ali Arman <ali@gmail.com>
2024-11-13 10:49:49 -0800

    Fixing bugs

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,8 @@ import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import axios from "axios";
 
-axios.defaults.withCredentials = true;
 axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
